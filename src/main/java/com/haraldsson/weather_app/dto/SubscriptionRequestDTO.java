6bb6121a2bb33f29175f7b0b30d501d2c26dad2c
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-12-01 13:48:23 +0100

    FIX: removed frequency from dto

--- src/main/java/com/haraldsson/weather_app/dto/SubscriptionRequestDTO.java
+++ src/main/java/com/haraldsson/weather_app/dto/SubscriptionRequestDTO.java
@@ -2,6 +2,5 @@ package com.haraldsson.weather_app.dto;
 
 public record SubscriptionRequestDTO(
         String city,
-        String timeOfDay,
-        String frequency
+        String timeOfDay
 ) {}
