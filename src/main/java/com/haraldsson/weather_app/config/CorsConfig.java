164e79b46bebb0f6192e48d29523404df668c243
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-12-01 15:28:50 +0100

    FIX: removed duplicate setallowedheaders

--- src/main/java/com/haraldsson/weather_app/config/CorsConfig.java
+++ src/main/java/com/haraldsson/weather_app/config/CorsConfig.java
@@ -20,7 +20,6 @@ public class CorsConfig {
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
 
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
